8d4a150ca4b4da160038dd607c223536bb71ca6f
Sergio <sergiosabogal1911@gmail.com>
2025-06-25 07:25:30 -0500

    Actualización

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,15 @@ public class Main {
         try {
             myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebadatos", "root", "Sq1007156509");
             System.out.println("CONEXIÖN ESTABLECIDA....");
+
+            myStamt  = myConn.createStatement();
+
+            myRes = myStamt.executeQuery("SELECT * FROM clients");
+            while (myRes.next()){
+                System.out.println(myRes.getString("name"));
+            }
+
+            
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("FALLA EN LA CONEXIÓN....");
